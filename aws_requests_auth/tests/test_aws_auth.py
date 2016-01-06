830144f0fcc0d346dd5d9578efe74c878ba22cf4
David Muller <davehmuller@gmail.com>
2016-01-06 08:47:10 -0800

    minor updates to test names and docstrings

--- aws_requests_auth/tests/test_aws_auth.py
+++ aws_requests_auth/tests/test_aws_auth.py
@@ -45,20 +45,20 @@ class TestAWSRequestsAuth(unittest.TestCase):
         self.assertEqual('/my_index/', AWSRequestsAuth.get_caononical_path(mock_request))
         self.assertEqual('pretty=True', AWSRequestsAuth.get_canonical_querystring(mock_request))
 
-    def test_multiply_get_params(self):
+    def test_multiple_get_params(self):
         """
         Assert we generate the 'correct' cannonical query string
-        for request that includes more than one parameter
+        for request that includes more than one query parameter
         """
         url = 'http://search-foo.us-east-1.es.amazonaws.com:80/index/type/_search?scroll=5m&search_type=scan'
         mock_request = mock.Mock()
         mock_request.url = url
         self.assertEqual('scroll=5m&search_type=scan', AWSRequestsAuth.get_canonical_querystring(mock_request))
 
-    def test_multiply_post_request_with_get_param(self):
+    def test_post_request_with_get_param(self):
         """
         Assert we generate the 'correct' cannonical query string
-        for request that includes GET-parameters
+        for a post request that includes GET-parameters
         """
         url = 'http://search-foo.us-east-1.es.amazonaws.com:80/index/type/1/_update?version=1'
         mock_request = mock.Mock()
