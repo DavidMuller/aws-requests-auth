84c465d0b06f9f14f1495c01368866bbce25bcd4
Yaron de Leeuw <me@jarondl.net>
2017-06-21 12:50:57 -0400

    add Python and Python3 classifiers to PyPI
    This will let me use brettcannon/caniusepython3

--- setup.py
+++ setup.py
@@ -11,4 +11,9 @@ setup(
     description='AWS signature version 4 signing process for the python requests module',
     long_description='See https://github.com/davidmuller/aws-requests-auth for installation and usage instructions.',
     install_requires=['requests>=0.14.0'],
+    classifiers=(
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        )
 )
