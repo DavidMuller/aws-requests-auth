8611621b2d84dbf73cdad1dd20b73db9da4e025e
Vasilyev, Viacheslav <viacheslav.vasilyev@accenture.com>
2019-01-17 12:12:35 +0200

    Allow to sign additional headers in request

--- aws_requests_auth/boto_utils.py
+++ aws_requests_auth/boto_utils.py
@@ -31,7 +31,7 @@ def get_credentials(credentials_obj=None):
 
 class BotoAWSRequestsAuth(AWSRequestsAuth):
 
-    def __init__(self, aws_host, aws_region, aws_service):
+    def __init__(self, aws_host, aws_region, aws_service, misc_sign_headers=None):
         """
         Example usage for talking to an AWS Elasticsearch Service:
 
@@ -43,11 +43,13 @@ class BotoAWSRequestsAuth(AWSRequestsAuth):
         automatically from the environment, in the order described here:
         http://boto3.readthedocs.io/en/latest/guide/configuration.html#configuring-credentials
         """
-        super(BotoAWSRequestsAuth, self).__init__(None, None, aws_host, aws_region, aws_service)
+        super(BotoAWSRequestsAuth, self).__init__(None, None, aws_host, aws_region, aws_service, misc_sign_headers=misc_sign_headers)
+        self._misc_sign_headers = misc_sign_headers
         self._refreshable_credentials = Session().get_credentials()
 
     def get_aws_request_headers_handler(self, r):
         # provide credentials explicitly during each __call__, to take advantage
         # of botocore's underlying logic to refresh expired credentials
         credentials = get_credentials(self._refreshable_credentials)
+        credentials['misc_sign_headers'] = self._misc_sign_headers
         return self.get_aws_request_headers(r, **credentials)
